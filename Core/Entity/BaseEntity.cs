12c6519687c1587ed10673afa12ac63d997162fc
omerfurkandogruyol <ko.omerfurkan@gmail.com>
2023-02-04 21:39:42 +0300

    Base entity Id modification

--- Core/Entity/BaseEntity.cs
+++ Core/Entity/BaseEntity.cs
@@ -4,7 +4,7 @@ namespace Core.Entity;
 
 public class BaseEntity<TId>
 {
-    public TId Id { get; set; }
+    public TId Id { get; set; } = default!;
     public Status Status { get; set; }
     public DateTime CreateDate { get; set; }
     public DateTime UpdateDate { get; set; }

58d7f90be4d75db1bca3ae9a68e0709e0cf57142
dogruyolomerfurkan <dogruyolomerfurkan@gmail.com>
2023-02-11 14:07:02 +0300

    MassTransit and RabbitMQ added to Consice Core

---
+++ Core/RabbitMQ/RabbitMQExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Core.RabbitMQ.Settings;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Core.RabbitMQ;
+
+public static class RabbitMQExtensions
+{
+    public static void AddMassTransitWithRabbitMQ(this IServiceCollection services)
+    {
+        services.AddMassTransit(configure =>
+        {
+            configure.AddConsumers(Assembly.GetEntryAssembly());
+            configure.UsingRabbitMq((context, configurator) =>
+            {
+                IConfiguration configuration = context.GetService<IConfiguration>()!;
+                ServiceSettings serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>()!;
+                RabbitMQSettings rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>()!;
+
+                configurator.Host(rabbitMQSettings.Host);
+                configurator.ConfigureEndpoints(context, KebabCaseEndpointNameFormatter.Instance);
+                configurator.UseMessageRetry(retryConfigurator => { retryConfigurator.Interval(3, TimeSpan.FromSeconds(5)); });
+            });
+        });
+    }
+}
---
+++ Core/RabbitMQ/Settings/RabbitMQSettings.cs
@@ -0,0 +1,3 @@
+﻿namespace Core.RabbitMQ.Settings;
+public class RabbitMQSettings { public string Host { get; init; } = null!; }
+public class ServiceSettings { public string Name { get; init; } = null!; }
